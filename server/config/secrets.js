6d0b1359da72bb8225df7f4f8974cdeb62564071
Preston <ludiculous89@gmail.com>
2016-01-29 14:50:03 -0800

    second

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,14 +1,14 @@
 /** Important **/
 /** You should not be committing this file to GitHub **/
 /** Repeat: DO! NOT! COMMIT! THIS! FILE! TO! YOUR! REPO! **/
-
+//'mongodb://ludiculous:123456@ds045785.mongolab.com:45785/reduxtribe',
 module.exports = {
   // Find the appropriate database to connect to, default to localhost if not found.
-  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/ReactWebpackNode',
+  db:  'mongodb://ludiculous:123456@ds045785.mongolab.com:45785/reduxtribe' ,
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
   google: {
-    clientID: process.env.GOOGLE_CLIENTID || '62351010161-eqcnoa340ki5ekb9gvids4ksgqt9hf48.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_SECRET || '6cKCWD75gHgzCvM4VQyR5_TU',
+    clientID: process.env.GOOGLE_CLIENTID || '648489384679-29jj5v10nm6pt9q28mkj582408aeepsh.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_SECRET || 'CQVIGNOtthUbvVtRcdbUciiu',
     callbackURL: process.env.GOOGLE_CALLBACK || "/auth/google/callback"
   }
 };
